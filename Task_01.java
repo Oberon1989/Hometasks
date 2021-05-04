9ba9ff1cd0fac25c058a495153f66a59cde8c1cd
Oberon1989 <reznikovskii@gmail.com>
2021-05-05 01:13:50 +0300

    1-е задание

--- Task_01.java
+++ Task_01.java
@@ -2,27 +2,30 @@ import java.lang.*;
 import java.util.*;
  class Task_01
 {
-	static int width;
-	static int length;
-	static int count;
+	static float width;
+	static float length;
+	static float count;
 	
 	
 	
 	public static void main(String[] args)
 	{
-		
+		String tmp="";
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Введите количество панелей");
-		count = scanner.nextInt();
+		tmp=scanner.nextLine().replace(',','.');
+		count = Float.parseFloat(tmp);
 		System.out.println("Введите длину панели");
-		length = scanner.nextInt();
+		tmp=scanner.nextLine().replace(',','.');
+		length = Float.parseFloat(tmp);
 		System.out.println("Введите ширину панели");
-		width = scanner.nextInt();
-		System.out.println(String.format("Для обработки указанного количество панелей (%s) с размером %s на %s метров необходимо %s нанограмм сульфида тория",count, width, length ,Calculate(width,length,count)));
+		tmp=scanner.nextLine().replace(',','.');
+		width = Float.parseFloat(tmp);
+		System.out.println(String.format("Для обработки указанного количество панелей (%s) с размером %s на %s метров необходимо %.2f нанограмм сульфида тория",count, width, length ,Calculate(width,length,count)));
 	}
-	public static int Calculate(int x,int y, int c)
+	public static float Calculate(float x,float y, float c)
 	{
-		return x*y*c;
+		return x*y*c*2;
 	}
 	
 	
