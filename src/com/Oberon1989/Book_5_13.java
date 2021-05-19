83fc0e2538f5d1b13a6205d1529a9638f7c56ee0
Oberon1989 <reznikovskii@gmail.com>
2021-05-19 17:00:58 +0300

    Add full multiplication table in range 2-9

--- src/com/Oberon1989/Book_5_13.java
+++ src/com/Oberon1989/Book_5_13.java
@@ -1,11 +1,50 @@
+package com.Oberon1989;
+
 public class Book_5_13 {
+    /*Мне показалось скучным вывести просто таблицу умножения на 7. Поэтому я решил вывести всю таблицу от 2 до 9
+
+    Код не оптимальный и присутствуют MAGIC_NUMBERS но это работает =)
+     */
     public static void main(String[] args) {
-        int i =1;
-        int number=7;
-        while(i<10)
-        {
-            System.out.println(String.format("%s x %s = %s",i,number,i*number));
-            i++;
+
+        System.out.println(multiplicationTable());
+    }
+    public static String multiplicationTable()
+    {
+        String result="";
+        int row = 4;
+        int start =1;
+        int count=2;
+        int tmp=count;
+        int columns=0;
+        while (columns<2) {
+
+            for(int k=1;k<10;k++) {
+                for (int i = 2; i < row+2; i++) {
+                    if(count*start>9)
+                    {
+                        result += String.format("%s * %s = %s   ",count,start,count*start);
+                    }
+                    else
+                    {
+                        result += String.format("%s * %s = %s    ",count,start,count*start);
+                    }
+
+                    count++;
+
+                }
+
+                count=tmp;
+
+                result+="\n";
+                start++;
+            }
+            result+="\n";
+            columns++;
+            count=6;
+            tmp=6;
+            start=1;
         }
+        return  result;
     }
 }
